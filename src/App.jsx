32c7e060d490e4525790874cb905b2ec37a05a55
praveenbollam9550@gmail.com <{praveenbollam9550@gmail.com}>
2026-02-24 22:38:29 +0530

    Final

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
 import 'bootstrap/dist/css/bootstrap.css';
@@ -18,7 +18,7 @@ import Food from "./Pages/api/Food";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
 
         <Routes>
@@ -36,6 +36,6 @@ export default function App() {
         </Routes>
 
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
